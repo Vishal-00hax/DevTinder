835d304ce53de2e7b247d48bc83bbb9e12459444
Vishal Panghal <vishalsinghpanghal@gmail.com>
2026-04-22 13:45:41 +0530

    Middelwares

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const {adminAuthMiddelware,userAuth} = require("../middlewares/AuthMiddelware");
 const app = express();
 const port = 7777;
 
-app.use('/admin', adminAuthMiddelware,userAuth);
+app.use('/admin', adminAuthMiddelware);
+app.use('/user', userAuth);
 
 app.get('/admin/getAllData',(req,res,next)=>{ 
 
@@ -17,6 +18,12 @@ app.get('/admin/getAllData',(req,res,next)=>{
  
         res.send("Data deleted successfully")
   
+ })
+
+ app.get('/user',(req,res,next)=>{ 
+
+        res.send("User data is here")
+
  })
 
 app.listen(port, ()=>{
