b2bb7a5572fce7dc68cf14fd914824ce111879f3
Vishal Panghal <vishalsinghpanghal@gmail.com>
2026-04-29 15:21:27 +0530

    Explore bycrypt and hashedPassword

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ const connectDB = require('./config/database');
 const app = express();
 const port = 7777;
 const User = require('./models/user');
+const bycrypt = require('bcrypt')
 
 app.use(express.json());
 
 app.post('/signup', async (req,res)=>{
 // console.log(req.body);
-const user  =  new User(req.body);
 try{
- await user.save();
+    const hashedPassword = await bycrypt.hash(req.body.password,10) // password hasing 
+
+    const user  =  new User({ ...req.body, password: hashedPassword});
+    await user.save();
 res.send("User Created successfully ✅")
 }
 catch(err){
@@ -21,7 +24,7 @@ catch(err){
         const message = Object.values(err.errors).map((val)=> val.message);
         return res.status(400).send(message);
     }
-    res.status(400).send("User Creating failed ",err);
+   return res.status(400).send("User Creating failed " + err.massage);
 }
 })
 
@@ -30,10 +33,10 @@ app.get('/feed', async (req,res)=>{
     const lname = req.query.lastName;
 try{
 const user = await User.find({firstName:fname , lastName:lname});
-res.send(user);
+  res.send(user);
 }
 catch(err){
-res.status(400).send("Error while fetching users",err);
+ res.status(400).send("Error while fetching users",err);
 }
 })
 
@@ -41,25 +44,25 @@ app.patch('/user/:user_id', async (req,res)=>{
     const userid = req.params.user_id;
     const update = req.body;
     try{
-        // const allowedUpdates = ["firstName", "lastName", "emailId", "password", "age"];
-        // const updateKeys = Object.keys(req.body);
-        // const isValidOpration = updateKeys.every((key)=> allowedUpdates.includes(key));
-        // if(!isValidOpration){
-        //     res.status(400).send("Invalid updates! Only firstName, lastName, emailId, password and age can be updated.")
-        // }
+         const allowedUpdates = ["firstName", "lastName", "age", "gender"];
+         const updateKeys = Object.keys(req.body);
+         const isValidOpration = updateKeys.every((key)=> allowedUpdates.includes(key));
+         if(!isValidOpration){
+           return res.status(400).send("Invalid updates! Only firstName, lastName, age , and geder can be updated.")
+         }
       const user = await User.findByIdAndUpdate(userid, update,{
         runValidators: true,
         returnDocument: "after"
       });
         if (!user){
-            res.send("User not Found")
+          return  res.send("User not Found")
         }
         else{
-            res.send("User updated sucessfuly")
+           return res.send("User updated sucessfuly")
         }
     }
     catch(err){
-        res.status(400).send("Error during user update" + err.message)
+       return res.status(400).send("Error during user update" + err.message)
     }
 })
 
@@ -69,10 +72,10 @@ const userid = req.params.user_id;
 try{
     const deleteUser = await User.findByIdAndDelete(userid);
     if(!deleteUser){
-        res.send("User not found for deletion")
+       return res.send("User not found for deletion")
     }
     else{
-        res.send("User deleted sucessfully")
+       return res.send("User deleted sucessfully")
     }
 }
 catch(err){
